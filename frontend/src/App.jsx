2ad21d8be16ec917143e3cca1c656ca83e594302
Abhiraj-cuchd <21bca1058@cuchd.in>
2023-11-15 00:57:36 +0530

    Project Initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600">Hello There Travel Agnecy Website</h1>
+    </div>
+  );
+};
+
+export default App;
