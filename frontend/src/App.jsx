91d3f52198060fd89996b044b0ee218ff7e97ea6
Abhiraj-cuchd <21bca1058@cuchd.in>
2023-11-15 01:32:53 +0530

    Header done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { BrowserRouter, Routes,  Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Login from "./Pages/Auth/Login";
 import Register from "./Pages/Auth/Register";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<Login />} />
-        <Route path="/sign-up" element={<Register />} />  
+        <Route path="/sign-up" element={<Register />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
