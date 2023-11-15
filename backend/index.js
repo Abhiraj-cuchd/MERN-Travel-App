cf2f5d05bfd18fd16412b72f5dd00da1a1430e70
Abhiraj-cuchd <21bca1058@cuchd.in>
2023-11-15 11:43:11 +0530

    Create User routes

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_DB).then(() => {
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
     console.log("Connected to TravellioDB");
-}).catch((err) => { 
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000!!!');
-});
+app.use('/api/user', userRouter);
 
+app.listen(3000, () => {
+  console.log("Server listening on port 3000!!!");
+});
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Travellio</span>
-            <span className="text-slate-700">Assam</span>
+            {/* <span className="text-slate-700">Assam</span> */}
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
